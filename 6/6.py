a00fb7f3dcc4c4dba17db134292cee749d6bb9cf
e-Access AITM <129937201+aitmaccess@users.noreply.github.com>
2024-07-27 13:03:05 +0530

    Create 6.py

---
+++ 6/6.py
@@ -0,0 +1,18 @@
+!pip install pgmpy 
+
+import pandas as pd
+from pgmpy.models import BayesianModel 
+from pgmpy.estimators import MaximumLikelihoodEstimator 
+from pgmpy.inference import VariableElimination 
+
+data = pd.read_csv('/content/ML LAB/datasetheart.csv', names=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'RESULT']) 
+
+print(data.head(5)) 
+print(data.tail(5)) 
+
+model = BayesianModel([("A", "B"), ("B", "C"), ("C", "D"), ("D", "RESULT")]) 
+model.fit(data, estimator=MaximumLikelihoodEstimator) 
+
+infer = VariableElimination(model) 
+q = infer.query(variables=['RESULT'], evidence={"C": 2}) 
+print(q)
