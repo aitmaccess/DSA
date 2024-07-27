6eb081ea6b1b84115399c39eb257e3cb7e1ae64e
e-Access AITM <129937201+aitmaccess@users.noreply.github.com>
2024-07-27 13:17:27 +0530

    Update 6.py

--- 6/6.py
+++ 6/6.py
@@ -5,7 +5,7 @@ from pgmpy.models import BayesianModel
 from pgmpy.estimators import MaximumLikelihoodEstimator 
 from pgmpy.inference import VariableElimination 
 
-data = pd.read_csv('/content/ML LAB/datasetheart.csv', names=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'RESULT']) 
+data = pd.read_csv('datasetheart.csv', names=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'RESULT']) 
 
 print(data.head(5)) 
 print(data.tail(5)) 
