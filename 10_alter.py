69b3243ab3da077246e5bfaaf4f934dd172f0e7f
e-Access AITM <129937201+aitmaccess@users.noreply.github.com>
2024-08-02 01:37:37 +0530

    Update 10_alter.py

--- 10_alter.py
+++ 10_alter.py
@@ -4,12 +4,12 @@ from sklearn.svm import SVC
 from sklearn.datasets import load_iris
 
 iris = load_iris()
-X_iris = iris.data[:, :2]
+x = iris.data[:, :2]
 y = iris.target 
 
 svm = SVC(kernel='linear')
-svm.fit(X_iris, y)
+svm.fit(x, y)
 
-DecisionBoundaryDisplay.from_estimator(svm, X_iris, response_method="predict")
-plt.scatter(X_iris[:, 0], X_iris[:, 1], c=y, s=20, edgecolors="k")
+DecisionBoundaryDisplay.from_estimator(svm, x, response_method="predict")
+plt.scatter(x[:, 0], x[:, 1], c=y, s=20, edgecolors="k")
 plt.show()
