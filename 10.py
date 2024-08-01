a6327e668595a1df6b3071ebcee418b4bbb4bffc
e-Access AITM <129937201+aitmaccess@users.noreply.github.com>
2024-08-02 01:44:11 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -8,21 +8,17 @@ X = cancer.data[:, :2]
 y = cancer.target
 
 svm = SVC(kernel="rbf", gamma=0.5, C=1.0)
-
 svm.fit(X, y)
 
-
 DecisionBoundaryDisplay.from_estimator(
-        svm,
-        X,
-        response_method="predict",
-        cmap=plt.cm.Spectral,
-        alpha=0.8,
-        xlabel=cancer.feature_names[0],
-        ylabel=cancer.feature_names[1],
-    )
+    svm,
+    X,
+    response_method="predict",
+    cmap=plt.cm.Spectral,
+    alpha=0.8,
+    xlabel=cancer.feature_names[0],
+    ylabel=cancer.feature_names[1]
+)
 
-plt.scatter(X[:, 0], X[:, 1], 
-            c=y, 
-            s=20, edgecolors="k")
+plt.scatter(X[:, 0], X[:, 1], c=y, s=20, edgecolors="k")
 plt.show()
