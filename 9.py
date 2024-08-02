ab6c4383a6084e9ab86136277b437faed34fb245
e-Access AITM <129937201+aitmaccess@users.noreply.github.com>
2024-08-02 07:13:05 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,15 +1,11 @@
-# 9
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Generating Data
 x = np.linspace(-5, 5, 1000)
 y = np.log(np.abs((x ** 2) - 1) + 0.5)
 x = x + np.random.normal(scale=0.05, size=1000)
 plt.scatter(x, y, alpha=0.3)
 
-
-# Locally Weighted Regression Functions
 def local_regression(x0, x, y, tau):
   x0 = np.r_[1, x0]
   x = np.c_[np.ones(len(x)), x]
@@ -17,18 +13,15 @@ def local_regression(x0, x, y, tau):
   beta = np.linalg.pinv(xw @ x) @ xw @ y
   return x0 @ beta
 
-
 def radial_kernel(x0, x, tau):
   return np.exp(np.sum((x - x0) ** 2, axis=1) / (-2 * tau ** 2))
-# Plotting Function
+
 def plot_lr(tau):
   domain = np.linspace(-5, 5, num=300)
   pred = [local_regression(x0, x, y, tau) for x0 in domain]
   plt.scatter(x, y, alpha=0.3)
   plt.plot(domain, pred, color="blue")
-  plt.show() # Corrected show() function
+  plt.show()
   return plt
 
-
-# Plotting with specified bandwidth (tau)
 plot_lr(0.03)
